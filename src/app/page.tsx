21d989bdc6d96371b90939ba8e0bf29582727f46
Bart Wttewaall <bart.wttewaall@divotion.com>
2025-02-04 16:49:20 +0100

    Add vitest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { ProductsList } from '@/components/productsList';
 import { HeaderTop } from '@/components/headerTop';
 
-export default async function Home() {
+export default function Home() {
   return (
     <>
       <HeaderTop />
       <main className="container mx-auto">
+        <h2>HELLO: {process.env.HELLO}</h2>
         <ProductsList />
       </main>
     </>
